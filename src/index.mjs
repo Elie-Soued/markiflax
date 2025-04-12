f6321d9b6baa43a6e23dfce94d2083dc5b7f5f52
Elie Soueid <eliegebransoued@gmail.com>
2025-04-12 12:58:06 +0200

    make sure port can be configured

--- src/index.mjs
+++ src/index.mjs
@@ -3,8 +3,14 @@
 import express from "express";
 import routes from "./routes.mjs";
 import path from "path";
+import { readFileSync } from "fs";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const cwd = process.cwd();
+const config = JSON.parse(readFileSync(`${cwd}/config.json`, "utf-8"));
+
+const { port } = config;
 
 const app = new express();
 
@@ -18,4 +24,4 @@ app.use("/", routes);
 
 app.get("/", (_, res) => res.send("Hello World"));
 
-app.listen(3000, () => console.log("Markiflax is running on port 3000"));
+app.listen(port, () => console.log(`Markiflax is running on port ${port}`));
